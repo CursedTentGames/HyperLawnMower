696d5d217b24c760f1898c79f7b87b992295645c
Aisterino <aistismilieska@gmail.com>
2021-09-23 20:53:54 +0300

    Added read me script that will work as documentation for developers

---
+++ [Lunatics] LawnMower/Assets/Scripts/Notepads/readme.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class readme : MonoBehaviour
+{
+    #region If there's a need to change default robots movement speed or rotation:
+    /*
+    1. Open my "RobotsStorage" script
+    2. You should see a region called "Adding Robots to the List", if there's a "+" sign on that line press it
+    3. You should see few robot class declarations. For exaple "firstRobot = new Robot(...) etc.
+    Hover on word "Robot" it should be colored in green. You should see tooltip with passed variable types and names.
+    It will help you understand the meaning of numbers passed in constructor.
+    4. Now just change the numbers to values you need.
+    
+    !!!Oh btw defaul robot is "firstRobot"!!!
+    */
+    #endregion
+}
